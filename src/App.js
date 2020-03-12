dca844c1e9079261dab763ccc664dd123858cfdb
mkarkihalli <meghakarkihalli@example.com>
2020-03-12 00:49:18 -0700

    Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
-import React from 'react';
-import SideNavBar from './side-nav-bar'
-import TopMenuBar from './top-menu-bar'
+import React from "react";
+import SideNavBar from "./side-nav-bar";
+import TopMenuBar from "./top-menu-bar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import AccordionPage from "./route-pages/accordion";
+import Image from "./logo.png";
+
+import "./_style.scss";
+
+const blk = "side-nav-bar";
 
 function App() {
   return (
-    <div className="App">
-      <TopMenuBar />
-      <SideNavBar />
-    </div>
+    <Router>
+      <div className="App">
+        <div className="top-menu">
+          <TopMenuBar />
+        </div>
+        <div className="content">
+          <div className="side-menu">
+            <div className={`${blk}__container`}>
+              <div className={`${blk}__icon-container`}>
+                <img src={Image} className={`${blk}__logo `} />
+                <span className={`${blk}__logo-text`}>CSS Library</span>
+              </div>
+              <div>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/users">Users</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+          <div className="main">
+            <Switch>
+              <Route exact path="/accordion" component={AccordionPage} />
+              <Route path="/about">
+                <h2>Hello</h2>
+              </Route>
+              <Route path="/dashboard">
+                <h3>Hello</h3>
+              </Route>
+            </Switch>
+          </div>
+        </div>
+        {/* <div className="footer">
+          <h1>Footer</h1>
+        </div> */}
+      </div>
+    </Router>
   );
 }
 
--- src/top-menu-bar/index.js
+++ src/top-menu-bar/index.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
-import FontAwesome from 'react-fontawesome'
+import React, { Component } from "react";
+import FontAwesome from "react-fontawesome";
 
-import './_style.scss'
+import "./_style.scss";
 
 class TopMenuBar extends Component {
   render() {
-    const blk = 'top-menu-bar'
+    const blk = "top-menu-bar";
     return (
       <div className={`${blk}__container`}>
         <button className={`${blk}__button`} onClick={this.signInModal}>
-          <FontAwesome
-            name="user"
-            size="2x"
-          />
+          <FontAwesome name="user" size="2x" />
           <span className={`${blk}__text`}>Sign In</span>
         </button>
       </div>
-    )
+    );
   }
 }
 
-export default TopMenuBar
+export default TopMenuBar;
